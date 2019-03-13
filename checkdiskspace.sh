655fca369d247e4d2403b08f8e1025adf874ecaf
sanyal06 <47888745+sanyal06@users.noreply.github.com>
2019-03-13 17:24:36 +0530

    Update checkdiskspace.sh

--- checkdiskspace.sh
+++ checkdiskspace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 MAX=80
-EMAIL=maheshdongare1983@gmail.com
+EMAIL=traningsanyal06@gmail.com
 PART=sda1
 USE=`df -h |grep $PART | awk '{ print $5}' |cut -d'%' -f1`
 if [ $USE -gt $MAX ]; then
